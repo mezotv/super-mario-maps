099bd2b8b313a87c3aed427c6f0bc6a0b17bcf27
mezotv <68947960+mezotv@users.noreply.github.com>
2021-04-11 00:28:50 +0200

    Added everything

---
+++ script.js
@@ -0,0 +1,7 @@
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 34.66779239799228, lng: 135.43051711370504},
+        zoom: 18,
+        mapId: '48babd97ac365492'
+    });
+}
